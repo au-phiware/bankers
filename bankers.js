6176e170e4271939a39c6baa38d08fb86b7ec776
au-phiware <corin@phiware.com.au>
2011-12-18 09:34:35 +0000

    Added Bankers and Binom

---
+++ bankers.js
@@ -0,0 +1,63 @@
+if (String.prototype.reverse == undefined)
+    String.prototype.reverse = function(){
+        return this.split("").reverse().join("");
+    }
+
+var Bankers = {
+    next: function(b) {
+        var z = "", y = "", next = "";
+        
+        var comp = /(1*)(0*)(1[01]*)?/.exec(b.reverse());
+        comp.reverse();
+        
+        if (comp.length > 1 && comp[1])
+            z = comp[1];
+        if (comp.length > 2 && comp[2])
+            y = comp[2];
+        if (comp[0])
+            next = comp[0].reverse().substring(0, comp[0].length - 1) + "0";
+            
+        next += y + "1";
+        if (z.length > 0)
+            next += z.substring(1);
+            
+        return next;
+    },
+    to: function(a, n) {
+        if (typeof a == "string") {
+            n = n || a.length;
+            a = parseInt(a, 2);
+        }
+        var c = 0, b = 0, e = 0;
+        if (a == 0)
+            return 0;
+            
+        do {
+            a -= Binom.choose(n, c++);
+        } while (Binom.choose(n, c) <= a);
+        c--;
+        e = a;
+        n--;
+        if (Binom.choose(n, c))
+            b++;
+        while (c-- >= 0) {
+            if (b % 2 == 0)
+                e -= Binom.choose(n, c);
+            b << 1;
+            if (Binom.choose(n-- + 1, c)) 
+                b++;
+        }
+        return b;
+    },
+    from: function(b) {
+        var bits = b.split("");
+        var n = b.length, a = 0, c = 0, cn = b.match(/1/g).length;
+        for (var i = 0; i < bits.length && c < cn; i++) {
+            if (bits[i] == "1")
+                a += Binom.choose(n, c++);
+            else
+                a += Binom.choose(n - i - 1, cn - c - 1);
+        }
+        return a;
+    }
+}
---
+++ binom.js
@@ -0,0 +1,15 @@
+var Binom = {
+    choose: function(n, k) {
+        if (n < 0 || k < 0)
+            throw "Unable to choose less than zero.";
+        if (n == 0) return 0;
+        if (n == k || k == 0) return 1;
+        return this.choose(n - 1, k - 1) + this.choose(n - 1, k);
+   },
+   sum: function(n, k) {
+       if (k == 0) return 1;
+       return this.sum(n, k - 1) + this.choose(n, k);
+   }
+};
+
+Binom.version = "0.1"
